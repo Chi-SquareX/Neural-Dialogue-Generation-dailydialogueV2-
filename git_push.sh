a6c17b59b37a2ddcd959e12f7177a81480f3f421
Goldenprince8420 <rahulgolder0202@gmail.com>
2022-10-26 00:18:45 +0530

    Materials commit

---
+++ git_push.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Materials commit"
+git remote add origin https://github.com/Chi-SquareX/Neural-Dialogue-Generation-dailydialogueV2-.git
+git branch -M main
+git push -u origin
