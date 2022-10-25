1115ac9bba4a1a5ea3d8285091399340a8efc050
Goldenprince8420 <rahulgolder0202@gmail.com>
2022-10-26 00:21:20 +0530

    Materials commit

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,6 @@
 git init
 git add .
 git commit -m "Materials commit"
-git remote add origin https://github.com/Chi-SquareX/Neural-Dialogue-Generation-dailydialogueV2-.git
+# git remote add origin https://github.com/Chi-SquareX/Neural-Dialogue-Generation-dailydialogueV2-.git
 git branch -M main
 git push --set-upstream origin main
